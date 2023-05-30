8de07a80627d29398e4e865fc535b436a7df0685
yj <jdudwo@gmail.com>
2023-05-30 17:11:59 +0900

    add Controller

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    String helloName(@RequestParam String name){
+        return "Hello " + name;
+    }
+}
